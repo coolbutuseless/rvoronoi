45875b8e02cce53fbe8e4c3ac923015bc18b7f49
mikefc <coolbutuseless@users.noreply.github.com>
2024-09-18 01:21:22 +0000

    test and avoid errors at low point counts

---
+++ tests/testthat/test-delaunay-low-points.R
@@ -0,0 +1,30 @@
+
+test_that("Delaunays with low point counts behave sanely", {
+
+  set.seed(1)
+  
+  N <- 0
+  x <- runif(N)
+  y <- runif(N)
+  del <- delaunay(x, y)
+  expect_equal(del$ntris, 0)
+  
+  N <- 1
+  x <- runif(N)
+  y <- runif(N)
+  del <- delaunay(x, y)
+  expect_equal(del$ntris, 0)
+  
+  N <- 2
+  x <- runif(N)
+  y <- runif(N)
+  del <- delaunay(x, y)
+  expect_equal(del$ntris, 0)
+  
+  N <- 3
+  x <- runif(N)
+  y <- runif(N)
+  del <- delaunay(x, y)
+  expect_equal(del$ntris, 1)
+  
+})
---
+++ tests/testthat/test-voronoi-low-points.R
@@ -0,0 +1,34 @@
+
+test_that("Voronois with low point counts behave sanely", {
+
+  set.seed(1)
+  
+  N <- 0
+  x <- runif(N)
+  y <- runif(N)
+  expect_no_error({
+    vor <- voronoi(x, y)
+  })
+  
+  N <- 1
+  x <- runif(N)
+  y <- runif(N)
+  expect_no_error({
+    vor <- voronoi(x, y)
+  })
+  
+  N <- 2
+  x <- runif(N)
+  y <- runif(N)
+  expect_no_error({
+    vor <- voronoi(x, y)
+  })
+  
+  N <- 3
+  x <- runif(N)
+  y <- runif(N)
+  expect_no_error({
+    vor <- voronoi(x, y)
+  })
+  
+})
